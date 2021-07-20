c422c77c5621618ce837a1ce5549f241691610c1
Israel Fleetwood <67342332+hgsisrael@users.noreply.github.com>
2021-07-20 20:17:58 +0000

    done ...

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
-
-// Global ML Variables
-//let mobilenet;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let mobilenet;
+let img;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  img = loadImage("images/guinea-pig.jpg");
+  img = loadImage("images/guinea-pig.jpg", imageLoaded);
+  
 }
 
 function draw() {
@@ -15,13 +25,22 @@ function draw() {
 }
 
 function imageLoaded() {
-
+  image(img, 0, 0, width, height);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  mobilenet.classify(canvas, gotResults);
 }
 
 function modelReady() {
-
+  mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
+  function gotResults(error, results) {
+  if(error) {
+    console.error(error);
+  } else {
+    console.log(results);
+  }
 
+}
 }
